ede2dd7edae72b203fce2cfc00ea9e876e7f893e
Madhav Agarwal <madie@Madhavs-MacBook-Air.local>
2024-11-28 14:44:21 +0530

    second code file added.

---
+++ Java-program-code/B_first_alphabet_capital_check.java
@@ -0,0 +1,16 @@
+// take an input from user and check wether if its capital letter or not.
+import java.util.Scanner;
+public class B_first_alphabet_case_check {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input the character from (a-z) or (A-Z): ");
+        char character = input.next().trim().charAt(0); 
+        //trim() it trims the spacing
+        //charAt()means give the character at ()index.
+        if(character >= 'a' && character <='z'){
+            System.out.println("it's smallcase.");
+        }if (character>='A' && character<='Z') {
+            System.out.println("its uppercase.");
+        }
+    }
+}
